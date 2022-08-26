8b91a00c32436bdc73bd5c371aea24951bab08a6
imabby0508 <imabby0508@gmail.com>
2022-08-27 01:14:16 +0800

    add checkout js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import "./scss_dir/main.scss";
 
-// const shippingWrapper = document.querySelectorAll(".shipping-wrapper");
-const shippingRadio = document.querySelectorAll(".shipping-radio");
+const cartItemsWrapper = document.querySelector(".cart-items-wrapper");
+const shippingRadioBtns = document.querySelectorAll('input[name="shipping"]');
+const totalShippingFee = document.querySelector(".total-shipping-fee");
+const totalAmount = document.querySelector(".total-amount");
 const stepperPanel = document.querySelector(".stepper-panel");
 const steps = stepperPanel.querySelectorAll(".step");
 const form = document.querySelector(".form");
@@ -12,10 +14,96 @@ const prevBtn = btnPanel.querySelector(".btn-previous");
 const finishBtn = btnPanel.querySelector(".btn-finish");
 
 let step = 0;
+let shippingFee = 0;
+let checkoutAmount = 0;
 
-function shippingMethodClicked(e) {
-  console.log(e.target);
-  // shippingRadio[0].classList.add("checked");
+let cartItems = [
+  {
+    id: 0,
+    name: "破壞補丁修身牛仔褲",
+    image: "/src/image/cart-item-1.png",
+    quantity: 1,
+    price: 3999,
+  },
+  {
+    id: 1,
+    name: "刷色直筒牛仔褲",
+    image: "/src/image/cart-item-2.png",
+    quantity: 1,
+    price: 1299,
+  },
+];
+
+function renderCartItems() {
+  cartItems.forEach((item) => {
+    cartItemsWrapper.innerHTML += `
+    <div class="cart-item-wrapper d-flex" id="${item.id}">
+      <img
+        src="${item.image}"
+        alt="cart-item"
+        class="cart-item__image"
+      />
+      <div class="cart-item-details d-flex">
+        <p class="cart-item__name">${item.name}</p>
+        <div class="cart-item-quantity-wrapper d-flex">
+          <div class="minus-container" id="${item.id}">-</div>
+          <div class="cart-item__quantity">${item.quantity}</div>
+          <div class="plus-container" id="${item.id}">+</div>
+        </div>
+        <p class="cart-item__price">$${item.price.toLocaleString("en-US")}</p>
+      </div>
+    </div>
+    `;
+  });
+}
+
+function cartItemsClicked(e) {
+  const id = e.target.id;
+  const parentElement = e.target.parentElement;
+  let quantity = 0;
+  let subAmount = 0;
+  if (e.target.matches(".plus-container")) {
+    quantity = cartItems[id].quantity += 1;
+    subAmount = cartItems[id].price * quantity;
+  } else if (
+    e.target.matches(".minus-container") &&
+    cartItems[id].quantity > 0
+  ) {
+    quantity = cartItems[id].quantity -= 1;
+    subAmount = cartItems[id].price * quantity;
+  } else {
+    return;
+  }
+  parentElement.children[1].innerHTML = quantity;
+  parentElement.nextElementSibling.innerHTML = `$${subAmount.toLocaleString(
+    "en-US"
+  )}`;
+  checkoutTotalAmount();
+}
+
+function shippingChosen(e) {  
+  for (const shippingRadioBtn of shippingRadioBtns) {
+    shippingRadioBtn.addEventListener("change", changeShippingFee)
+  }
+}     
+
+function changeShippingFee(e) {
+  if (this.checked && this.value > 0) {
+    totalShippingFee.innerHTML = `$${this.value}`;
+  } else {
+    totalShippingFee.innerHTML = "免費";
+  }  
+  shippingFee = Number(this.value);
+  checkoutTotalAmount();
+}
+
+function checkoutTotalAmount(e) {
+  let sum = 0;
+  for (let i = 0; i < cartItems.length; i++) {
+    sum += cartItems[i].quantity * cartItems[i].price;
+  }
+  checkoutAmount = sum + shippingFee;
+  totalAmount.innerHTML = `$${checkoutAmount.toLocaleString("en-US")}`;
 }
 
 function btnPanelClicked(e) {
@@ -57,5 +145,9 @@ function setBtnDisabled() {
   }
 }
 
-form.addEventListener("click", shippingMethodClicked);
+renderCartItems();
+checkoutTotalAmount();
+
+cartItemsWrapper.addEventListener("click", cartItemsClicked);
+form.addEventListener("click", shippingChosen);
 btnPanel.addEventListener("click", btnPanelClicked);
